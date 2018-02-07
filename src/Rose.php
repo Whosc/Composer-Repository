02c810ac62c7dc52f6a217f3a461ba94b194424f
Whosc <1421851519@qq.com>
2018-02-07 14:50:00 +0800

    M1 trackingmore track package 第一个版本测试

---
+++ src/Rose.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lingan
+ * Date: 2017/11/6
+ * Time: 21:39
+ */
+
+namespace M1\Track;
+
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
